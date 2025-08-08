9a289918716206e47c4939ee6aaa61eeafce7f0b
Boris <boris.komarov@proton.me>
2025-08-08 12:47:30 +0200

    scripts handle added to install script

--- install.bash
+++ install.bash
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+
+
+# Binaries
 
 TOOLS=(
 	stopwatch
@@ -8,5 +12,21 @@ TOOLS=(
 for tool in "${TOOLS[@]}"; do
     sudo cp "./bin/$tool" /usr/local/bin/
 done
+
+
+
+# Scripts
+
+SCRIPTS=(
+	wayscr
+)
+
+sudo pacman -S grim slurp --needed # wayscr dependencies
+
+for tool in "${SCRIPTS[@]}"; do
+    sudo cp "./scripts/$tool" /usr/local/bin/
+done
+
+
 
 echo "Done"
